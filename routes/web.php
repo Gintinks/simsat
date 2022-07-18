934d0a466d62fce0bd812e269a1d61f87f18cd9b
yudirudja <yudirudja2900@gmail.com>
2022-07-18 21:26:45 +0700

    admin fix

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ Route::post('/delete', [App\Http\Controllers\AdminRegisterController::class, 'de
 Route::get('/userList', [AdminRegisterController::class, 'viewUser'])->name('userList')->middleware('is_admin');
 // Route::post('/register', [App\Http\Controllers\AdminRegisterController::class, 'viewUser'])->name('viewUser')->middleware('is_admin');
 
-Route::get('/dashboard', [HomeController::class, 'index'])->name('dashboard');
+Route::get('/dashboard', [HomeController::class, 'index'])->name('dashboard')->middleware('is_tps_managementdlh');
 Route::get('/admin', [HomeController::class, 'admin'])->name('admin');
 
 // Route::get('/userList', [AdminController::class, 'index'])->name('userList')->middleware('is_admin');
@@ -48,7 +48,7 @@ Route::get('/onlyManagemenDlh', [ManagemenDlhController::class, 'index'])->name(
 Route::get('/sampahList', [SampahController::class, 'showSampahTps'])->name('sampahList')->middleware('is_tps_managementdlh');
 Route::get('/sampah-list', [SampahController::class, 'showHalamanSampahTps'])->name('sampahList')->middleware('is_tps_managementdlh');
 
-Route::post('/sampah-filter', [SampahController::class, 'sampahFilter'])->name('sampahFilter')->middleware('is_tps_managementdlh');
+Route::get('/sampah-filter', [SampahController::class, 'sampahFilter'])->name('sampahFilter')->middleware('is_tps_managementdlh');
 
 Route::get('/sampahInputView', [SampahController::class, 'indexInput'])->name('sampahInputView')->middleware('is_tps');
 Route::get('/sampahInput', [SampahController::class, 'storeSampah'])->name('sampahInput');
